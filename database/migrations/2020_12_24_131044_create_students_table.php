b547edeb0bd67ee481deb82ea773e918b306f9cb
Bapi Dey <devbapidey@gmail.com>
2020-12-24 19:05:05 +0530

    Created Student Model, Controller, Migration. Migration ready others untouched.

---
+++ database/migrations/2020_12_24_131044_create_students_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->string('blood_group', 3);
+            $table->string('mother_tongue', 12);
+            $table->bigInteger('parent_id')->unsigned();
+            $table->bigInteger('different_address')->unsigned();
+
+            // Todo There might be a class_section or student_class_year
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
